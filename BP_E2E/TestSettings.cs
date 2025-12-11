cd7feb8d9cdcf9556d2fd8998682c8945353b407
Amarnath <amarcs062002@gmail.com>
2025-12-11 22:48:13 +0000

    added E2E Test

---
+++ BP_E2E/TestSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP_E2E
+{
+    public static class TestSettings
+    {
+        
+        public static string BaseUrl =>
+            Environment.GetEnvironmentVariable("BASE_URL")
+            ?? "http://localhost:53135"; 
+    }
+}
